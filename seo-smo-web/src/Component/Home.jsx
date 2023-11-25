888585a93ad7b8d4e7468a49e849a4a738c459d4
Anuradhagupta28 <annugupta8512@gmail.com>
2023-11-25 20:18:23 +0530

    update homepage

--- seo-smo-web/src/Component/Home.jsx
+++ seo-smo-web/src/Component/Home.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import "../App.css";
 import location from "../assets/location.png"
+import smartphone from "../assets/smartphone.png"
+
 import mobile from "../assets/mobilePreview.png";
 function Home() {
   return (
     <div className="bg-blue-500 text-white p-4" >
    <div className='flex flex-row  p-6 '> 
-    <div style={{ width: '75%', height: '100%' ,border:"solid black"}} className='m-12 '>
+    <div style={{ width: '55%' ,border:"solid black"}} className='m-12 '>
       <h1 className="text-5xl font-bold">Digital Marketing Company In India</h1>
       <p className="mt-8 text-3xl ">Best Digital Marketing Services In India From Top Rated Digital Marketing Experts</p>
       <p className='mt-8 text-2xl '>A Well-Known Digital Marketing Company in India focused on Client Business Growth Only. Our digital marketing experts are offering Digital Marketing Solutions for businesses for more than 10 Years.</p>
-      <div>
-        <img src={location}/>
+      <div className='flex justify-between m-12'>
+       <div className='flex'>
+       <img  className='w-14 h-14' src={location}/>
+       <p> Noida , UP</p>
+       </div>
+
+       <div  className='flex '>
+       <img  className='w-14 h-14' src={smartphone}/>
+       <p>More Information Call Us</p>
+       </div>
       </div>
       </div>
 
